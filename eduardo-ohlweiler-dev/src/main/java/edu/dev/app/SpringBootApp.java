56a93afea57b43f600abfed42eb6dfd104d209ee
afgale <alex.fernando.egidio@gmail.com>
2025-11-19 16:22:11 -0300

    Projeto de estudo

---
+++ eduardo-ohlweiler-dev/src/main/java/edu/dev/app/SpringBootApp.java
@@ -0,0 +1,17 @@
+package edu.dev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "edu.dev.*")
+@EntityScan(basePackages = "edu.dev.entity")
+@ComponentScan(basePackages = { "edu.dev.*" })
+@EnableJpaRepositories(basePackages = { "edu.dev.repository" })
+public class SpringBootApp {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp.class, args);
+	}
+}
