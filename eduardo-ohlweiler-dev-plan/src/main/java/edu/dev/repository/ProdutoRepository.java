0860d04b844e89657e3096d18f4f101adab9644b
afgale <alex.fernando.egidio@gmail.com>
2025-11-26 16:40:24 -0300

    d

--- eduardo-ohlweiler-dev-plan/src/main/java/edu/dev/repository/ProdutoRepository.java
+++ eduardo-ohlweiler-dev-plan/src/main/java/edu/dev/repository/ProdutoRepository.java
@@ -1,11 +1,20 @@
 package edu.dev.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.dev.entity.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	@Transactional(readOnly = true)
+	@Query("SELECT p FROM Produto p WHERE LOWER(p.nome) LIKE %:nome%")
+	List<Produto> buscarPorNome(@Param("nome") String nome);
 
 }
