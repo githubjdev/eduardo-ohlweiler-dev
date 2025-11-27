de65c268ed8c2d8501e32925696650ebba1ea553
afgale <alex.fernando.egidio@gmail.com>
2025-11-26 21:48:20 -0300

    base de classes para teste

---
+++ eduardo-ohlweiler-dev/src/test/java/edu/dev/controller/ProdutoTeste.java
@@ -0,0 +1,72 @@
+package edu.dev.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import edu.dev.app.SpringBootApp;
+import edu.dev.entity.Produto;
+import edu.dev.repository.ProdutoRepository;
+import edu.dev.service.ProdutoService;
+import edu.dev.test.TesteGeneric;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = SpringBootApp.class, 
+               webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProdutoTeste extends TesteGeneric {
+    
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	@DisplayName("1 - GET /api/produtos - Deve retornar lista de produtos")
+	void testeListarTodos1() {
+		
+		produtoRepository.deleteAll();
+		
+		Produto p1 = new Produto("Produto A");
+		Produto p2 = new Produto("Produto B");
+		
+		p1 = produtoService.salvar(p1);
+		p2 = produtoService.salvar(p2);
+		
+		
+		ResponseEntity<Produto[]> response = 
+				                  restTemplate.getForEntity(url("api/produtos"), 
+				                   Produto[].class);
+		
+		List<Produto> produtos = Arrays.asList(response.getBody());
+		
+		assertEquals(2, produtos.size());
+		assertEquals(p1.getId(), produtos.get(0).getId());
+		assertEquals(p2.getId(), produtos.get(1).getId());
+		
+		
+		assertEquals(p1.getNome(), produtos.get(0).getNome());
+		assertEquals(p2.getNome(), produtos.get(1).getNome());
+		
+	}
+	
+	
+}
