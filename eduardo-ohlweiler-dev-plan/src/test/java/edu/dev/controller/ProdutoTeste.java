f1ea5810da6c47ff25cd8610a39bc03b2dcb9ff4
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-10 15:37:23 -0300

    .

--- eduardo-ohlweiler-dev-plan/src/test/java/edu/dev/controller/ProdutoTeste.java
+++ eduardo-ohlweiler-dev-plan/src/test/java/edu/dev/controller/ProdutoTeste.java
@@ -1,6 +1,10 @@
 package edu.dev.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import edu.dev.app.SpringBootApp;
@@ -61,6 +70,149 @@ public class ProdutoTeste extends TesteGeneric {
 		assertEquals(p2.getNome(), produtos.get(1).getNome());
 		
 	}
+	
+	/*FALTA PASSAR PARA O ALUNO*/
+	@Test
+	@DisplayName("2 - GET /api/produtos - Deve retornar lista de produtos")
+	void testeListarTodos2() throws Exception {
+
+		produtoRepository.deleteAll();
+
+		Produto p1 = new Produto("Produto A");
+		Produto p2 = new Produto("Produto B");
+
+		p1 = produtoService.salvar(p1);
+		p2 = produtoService.salvar(p2);
+
+		getMockMvc().perform(get("/api/produtos"))
+		              .andExpect(status().isOk())
+		              .andExpect(jsonPath("$.length()").value(2))
+				      .andExpect(jsonPath("$[0].id").value(p1.getId()))
+				      .andExpect(jsonPath("$[0].nome").value(p1.getNome()))
+				      .andExpect(jsonPath("$[1].id").value(p2.getId()))
+				      .andExpect(jsonPath("$[1].nome").value(p2.getNome()));
+
+	}
+
+	/*FALTA PASSAR PARA O ALUNO*/
+	@Test
+	@DisplayName("3 - GET /api/produtos - Deve retornar lista vazia")
+	void testeListarTodos3() throws Exception {
+		
+		produtoRepository.deleteAll();
+
+
+		ResponseEntity<Produto[]> response = restTemplate
+				        .getForEntity(url("api/produtos"), 
+				        Produto[].class);
+		
+		
+		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode().value());
+		assertEquals(0, response.getBody().length);
+		
+	}
+	
+	
+	/*FALTA PASSAR PARA O ALUNO*/
+	@Test
+	@DisplayName("4 - POST /api/produtos - Deve criar um novo produto")
+	void testeCriarProduto() throws Exception {
+		
+        Produto produto = new Produto();
+        produto.setNome("Teclado Mecânico");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Produto> request = new HttpEntity<>(produto, headers);
+
+        ResponseEntity<Produto> response =
+                restTemplate.postForEntity(url("api/produtos"), request, Produto.class);
+        
+        
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().getId());
+        assertEquals("Teclado Mecânico", response.getBody().getNome());
+        
+        produtoRepository.deleteById(response.getBody().getId());
+	}
+	
+	/*FALTA PASSAR PARA O ALUNO*/
+	@Test
+	@DisplayName("5 - PUT /api/produtos - Deve atualizar um produto")
+	void testeAtualizarProduto() throws Exception {
+		
+        Produto produto = new Produto();
+        produto.setNome("Teclado Mecânico");
+        
+        produto = produtoService.salvar(produto);
+        
+        /*Troca o nome para testar o atualizar*/
+        produto.setNome("Teclado Digital");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        ResponseEntity<Produto> response =
+                restTemplate.exchange(url("api/produtos"),
+                		     HttpMethod.PUT, 
+                		     new HttpEntity<>(produto, headers), 
+                		     Produto.class);
+        
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().getId());
+        assertEquals("Teclado Digital", response.getBody().getNome());
+        
+        produtoRepository.deleteById(response.getBody().getId());
+	}
+	
+	/*FALTA PASSAR PARA O ALUNO*/
+	@Test
+	@DisplayName("6 - GET /api/produtos/{id} - Deve buscar um produto pelo ID")
+	void testeBuscaPorId() throws Exception {
+		
+        Produto produto = new Produto();
+        produto.setNome("Teclado Mecânico");
+        
+        produto = produtoService.salvar(produto);
+        
+        ResponseEntity<Produto> response =
+                restTemplate.getForEntity(url("api/produtos/" + produto.getId()),
+                		Produto.class);
+        
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().getId());
+        assertEquals("Teclado Mecânico", response.getBody().getNome());
+        
+        produtoRepository.deleteById(response.getBody().getId());
+	}
+	
+	
+	/*FALTA PASSAR PARA O ALUNO*/
+	@Test
+	@DisplayName("8 - DELETE /api/produtos/{id} - Deve DELETER um produto pelo ID")
+	void testeDeletePorId() throws Exception {
+		
+        Produto produto = new Produto();
+        produto.setNome("Teclado Mecânico");
+        
+        produto = produtoService.salvar(produto);
+        
+        restTemplate.delete(url("api/produtos/" + produto.getId()));
+        
+        ResponseEntity<Produto> response =
+                        restTemplate.getForEntity(url("api/produtos/" + produto.getId()),
+                		Produto.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+	}
+
 	
 	
 }
